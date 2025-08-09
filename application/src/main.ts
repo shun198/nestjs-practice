63d4340d20ca11868e1eda0a879bcb2b88e8931f
shun198 <shunhiroseluvmri@gmail.com>
2025-08-09 09:35:41 +0900

    add auth modules

--- application/src/main.ts
+++ application/src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  const config = new DocumentBuilder().setTitle("NestJS API").setDescription('NestJS API desc').setVersion("1.0").build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, document);
+  await app.listen(8000);
 }
 bootstrap();

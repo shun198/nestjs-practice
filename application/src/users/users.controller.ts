7eeceeb3ea9a536e2caffe76ebb6935ebd05cf24
shun198 <shunhiroseluvmri@gmail.com>
2025-10-05 17:02:33 +0900

    include create and delete api for user

--- application/src/users/users.controller.ts
+++ application/src/users/users.controller.ts
@@ -2,15 +2,18 @@ import {
   Body,
   Controller,
   Get,
+  Delete,
   HttpCode,
   HttpStatus,
   Post,
   Request,
   Param,
   NotFoundException,
+  BadRequestException,
 } from '@nestjs/common';
 import { ApiTags, ApiBody, ApiParam, ApiResponse } from '@nestjs/swagger';
 import { UsersService } from './users.service';
+import { CreateUserDto } from '../entity/create-user.dto';
 
 @ApiTags('users')
 @Controller('users')
@@ -61,11 +64,63 @@ export class UsersController {
     description: 'User ID',
     schema: { type: 'integer', example: 1 },
   })
-  getUser(@Param() id: number) {
-    const user = this.usersService.findOneById(id);
+  async getUser(@Param('id') id: number) {
+    const user = await this.usersService.findOneById(id);
     if (!user) {
       throw new NotFoundException('User not found');
     }
     return user
+  }
+
+  @HttpCode(HttpStatus.CREATED)
+  @Post()
+  @ApiBody({
+    type: CreateUserDto,
+    description: 'ユーザー作成用のリクエストボディ',
+    examples: {
+      user: {
+        value: {
+          username: 'wada',
+          password: 'changeme',
+        },
+      },
+    },
+  })
+  @ApiResponse({
+    status: HttpStatus.CREATED,
+    description: 'The user has been successfully created.',
+    content: {
+      'application/json': {
+        example: {
+          id: 4,
+          username: 'wada',
+          password: '$2b$10$UvFrY2ifg7hrWLIY8udaPe3HgApQAjquTlgJTPkt.1h8YMJATncsu',
+          isActive: false,
+        },
+      },
+    },
+  })
+  async createUser(@Body() createUserDto: CreateUserDto) {
+    const user = await this.usersService.findOneByUsername(createUserDto.username);
+    if (user) {
+      throw new BadRequestException('User already exists');
+    }
+    return await this.usersService.createUser(createUserDto);
+  }
+
+  @HttpCode(HttpStatus.NO_CONTENT)
+  @Delete(':id')
+  @ApiParam({
+    name: 'id',
+    required: true,
+    description: 'User ID',
+    schema: { type: 'integer', example: 1 },
+  })
+  async deleteUser(@Param('id') id: number) {
+    const user = await this.usersService.findOneById(id);
+    if (!user) {
+      throw new NotFoundException('User not found');
+    }
+    this.usersService.remove(id);
   }
 }
--- application/src/users/users.service.ts
+++ application/src/users/users.service.ts
@@ -2,7 +2,9 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
-import { User } from './user.entity';
+import { User } from '../entity/user.entity';
+import { CreateUserDto } from '../entity/create-user.dto';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class UsersService {
@@ -19,9 +21,19 @@ export class UsersService {
     return await this.usersRepository.findOne({where: { username: username },});
   }
 
-
   async findOneById(id: number): Promise<User> | null {
-    return await this.usersRepository.findOneBy({ id: id })
+    return await this.usersRepository.findOne({ where: { id: id } });
+  }
+
+  async createUser(createUserDto: CreateUserDto): Promise<User> {
+    // https://docs.nestjs.com/security/encryption-and-hashing
+    const salt = await bcrypt.genSalt();
+    const hashedPassword = await bcrypt.hash(createUserDto.password, salt);
+    const user = this.usersRepository.create({
+      ...createUserDto,
+      password: hashedPassword,
+    });
+    return await this.usersRepository.save(user);
   }
 
   async remove(id: number): Promise<void> {
